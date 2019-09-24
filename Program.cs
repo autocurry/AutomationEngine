60fdae34640d20a1615e1521c77054147bbf7d21
sandeep <sandeepsekhar555@gmail.com>
2019-09-24 21:20:39 +0530

    consoleapp created

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace AutomationEngine
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter Application Name");
+            var ApplicationName = Console.ReadLine();
+            Console.WriteLine("Enter Environment Name");
+            var EnvironmentName = Console.ReadLine();
+
+            Console.WriteLine(string.Format("ApplicationName= {0}, EnvironmentName= {1}",ApplicationName,EnvironmentName));
+
+        }
+    }
+}
